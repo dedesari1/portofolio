d53f1ffe87aaecb6b0795f23ce71cb48792077cc
Macbook <macbook@Macbooks-MacBook-Air.local>
2026-02-18 10:43:25 +0700

    Refacto Navbar.jsx for cleaner layout and better readability

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -175,8 +175,8 @@ useEffect(() => {
     // DARK MODE → TAMPIL MOON
    <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="20"
+    height="20"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
